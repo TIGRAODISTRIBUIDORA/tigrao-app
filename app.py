32c0f3d706bc0b358e0ae8cf9f253ca743b3f22c
TIGRAODISTRIBUIDORA <sodemilecem23@gmail.com>
2026-06-27 11:11:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -94,10 +94,15 @@ else:
     
     tab_pedido, tab_cadastro, tab_consulta_prod, tab_recebimento = st.tabs([
         "📋 Passar Pedido", "➕ Cadastrar Cliente", "🔍 Consultar Produtos", "👑 Recebimento Nelson (Central)"
-    ])
+       if is_admin:
+        tab_pedido, tab_cadastro, tab_consulta_prod, tab_recebimento = st.tabs([
+            "📋 Passar Pedido", "➕ Cadastrar Cliente", "🔍 Consultar Produtos", "👑 Recebimento Nelson (Central)"
+        ])
+    else:
+        tab_pedido, tab_cadastro, tab_consulta_prod = st.tabs([
+            "📋 Passar Pedido", "➕ Cadastrar Cliente", "🔍 Consultar Produtos"
+        ])
 
-    # --- ABA 1: PASSAR PEDIDO ---
-    with tab_pedido:
         st.subheader("1. Escolha o Cliente")
         lista_nomes_clientes = df_clientes["Nome"].dropna().astype(str).tolist()
         cliente_escolhido = st.selectbox("Selecione o Cliente Cadastrado:", lista_nomes_clientes)
